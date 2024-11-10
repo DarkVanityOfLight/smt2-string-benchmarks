64be6e1d218e4157cc99e199f9a6d204a0f2a39f
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2024-11-10 11:20:12 +0100

    chore: Reformat

--- main.py
+++ main.py
@@ -8,16 +8,17 @@ from typing import Tuple, List
 from typing import Iterator
 
 solvers = {
-        "cvc5": ["/home/lichtner/cvc5"],
-        "ostrich": ["/home/lichtner/ostrich/ostrich", "-logo", "-runtime", "+quiet"],
-        "z3alpha": ["/software/python/3.12.0/bin/python3.12", "/home/lichtner/z3alpha/smtcomp24/z3alpha.py"],
-        "z3noodler": ["/home/lichtner/z3-noodler"]
-    }
+    "cvc5": ["/home/lichtner/cvc5"],
+    "ostrich": ["/home/lichtner/ostrich/ostrich", "-logo", "-runtime", "+quiet"],
+    "z3alpha": ["/software/python/3.12.0/bin/python3.12", "/home/lichtner/z3alpha/smtcomp24/z3alpha.py"],
+    "z3noodler": ["/home/lichtner/z3-noodler"]
+}
 
 
 OUTPUT_DIR = "out"
 seperator = ","
-TIMEOUT = 60 # Timeout in seconds
+TIMEOUT = 60  # Timeout in seconds
+
 
 class LazyPathIterator:
     def __init__(self, path: str):
@@ -45,7 +46,6 @@ def benchmark_solver(solver_name: str, solver_command: List[str], input_file: st
 
     os.makedirs(os.path.dirname(output), exist_ok=True)
 
-
     perf_command = ["perf", "stat", *solver_command, input_file]
 
     with open(output, "w+") as f:
@@ -56,11 +56,9 @@ def benchmark_solver(solver_name: str, solver_command: List[str], input_file: st
             f.write(f"timeout")
 
 
-
 def worker(solver_name: str, pool: LazyPathIterator):
     for f in pool:
         benchmark_solver(solver_name, solvers[solver_name], f)
-
 
 
 def run(path):
@@ -74,14 +72,13 @@ def run(path):
     solver3 = "z3noodler"
     solver4 = "z3alpha"
 
-
     with open(f"{solver1}.log", "w+"):
         pass
 
-    with open(f"{solver2}.log","w+"):
+    with open(f"{solver2}.log", "w+"):
         pass
 
-    with open(f"{solver3}.log","w+"):
+    with open(f"{solver3}.log", "w+"):
         pass
 
     with open(f"{solver4}.log", "w+"):
@@ -92,18 +89,19 @@ def run(path):
     thread2 = threading.Thread(target=worker, args=(solver2, pool2))
     thread3 = threading.Thread(target=worker, args=(solver3, pool3))
     thread4 = threading.Thread(target=worker, args=(solver4, pool4))
-    
+
     # Start threads
     thread1.start()
     thread2.start()
     thread3.start()
     thread4.start()
-    
+
     # Wait for threads to complete
     thread1.join()
     thread2.join()
     thread3.join()
     thread4.join()
+
 
 if __name__ == "__main__":
     run("benchmarks/non-incremental")
